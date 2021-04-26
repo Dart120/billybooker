30566a8ea704e5eeb03c19eab200e256285f6612
Dart120 <irenitemi81@gmail.com>
2021-04-26 20:33:45 +0100

    git ignore added

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ const express = require('express')
 const puppeteer = require('puppeteer');
 
 
-const entities = new Entities();
-// run in a non-headless mode
-events = []
 
 const fetchEvents = async() => {
 
     const browser = await puppeteer.launch({
-        headless: false,
+        headless: true,
         devtools: true,
         'args': [
             '--no-sandbox',
@@ -25,44 +22,43 @@ const fetchEvents = async() => {
     await page.goto('https://apps.dur.ac.uk/bookings/book/type', {
         waitUntil: 'networkidle2'
     });
-    await page.waitForSelector('._50f7');
+    await page.waitForSelector('#username');
     console.log('Page Object is set up')
-    thing = await page.evaluate(() => {
-        links = Array.from(document.querySelectorAll('._50f7'))
-        here = links.map((elem) => 'https://www.facebook.com' + elem.parentNode.getAttribute('href'))
-        return here
-    })
-    console.log('Returning', [thing, page])
-    return [thing, page]
+    await page.type('input[name=username]', process.env.USERNAME, { delay: 20 })
+        //     thing = await page.evaluate(() => {
+        //         links = Array.from(document.querySelectorAll('._50f7'))
+        //         here = links.map((elem) => 'https://www.facebook.com' + elem.parentNode.getAttribute('href'))
+        //         return here
+        //     })
+        //     console.log('Returning', [thing, page])
+        //     return [thing, page]
 
 
+    // }
+
+
+
+    // for (let i = 0; i < t.length; i++) {
+    //     urll = t[i];
+    //     console.log(urll)
+    //     await page.goto(urll, {
+    //         waitUntil: 'networkidle2'
+    //     })
+    //     cover = await page.evaluate(() => document.querySelector('._3ojl').children[0].getAttribute('src'))
+    //     console.log(1)
+    //     date = await page.evaluate(() => document.querySelector('._5xhk').innerHTML)
+    //     console.log(2)
+    //     place = await page.evaluate(() => document.querySelectorAll('._xkh')[1].children[0].innerHTML)
+    //     console.log(3)
+    //     title = await page.evaluate(() => document.querySelector('._5gmx').innerHTML)
+    //     console.log(4)
+    //     events.push({
+    //         cover,
+    //         date,
+    //         place,
+    //         title: entities.decode(title)
+    //     })
 }
+fetchEvents()
 
-
-
-for (let i = 0; i < t.length; i++) {
-    urll = t[i];
-    console.log(urll)
-    await page.goto(urll, {
-        waitUntil: 'networkidle2'
-    })
-    cover = await page.evaluate(() => document.querySelector('._3ojl').children[0].getAttribute('src'))
-    console.log(1)
-    date = await page.evaluate(() => document.querySelector('._5xhk').innerHTML)
-    console.log(2)
-    place = await page.evaluate(() => document.querySelectorAll('._xkh')[1].children[0].innerHTML)
-    console.log(3)
-    title = await page.evaluate(() => document.querySelector('._5gmx').innerHTML)
-    console.log(4)
-    events.push({
-        cover,
-        date,
-        place,
-        title: entities.decode(title)
-    })
-}
-
-
-
-
-module.exports = router
+// }
